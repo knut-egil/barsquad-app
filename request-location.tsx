de0b65a0b3c49b028c0a80641bb9006158285da0
Knut Egil <knut.egil.vn@gmail.com>
2024-02-16 13:54:21 +0100

    Add is defined check

--- request-location.tsx
+++ request-location.tsx
@@ -57,58 +57,59 @@ const PermissionsButton = ({
   </View>
 );
 
-TaskManager.defineTask<{ locations: [Location.LocationObject] }>(
-  LOCATION_TASK_NAME,
-  async ({ data, error }) => {
-    if (error) {
-      // Error occurred - check `error.message` for more details.
-      console.error(
-        `An error occured in our background location task, error:${error?.message}`
-      );
-      return;
-    }
-    if (data) {
-      const { locations } = data;
-      // do something with the locations captured in the background
-      console.info(
-        `Retreived ${
-          locations.length
-        } locations from background location task! Locations: ${JSON.stringify(
-          data,
-          null,
-          2
-        )}`
-      );
-
-      // Send to our websocket/update via fetch request!
-      try {
-        const squadCode = SquadController.getSquadCode();
-        const username = SquadController.getUsername();
-
-        if (!squadCode) return;
-        if (!username) return;
-
-        const payload = {
-          username: username,
-          locations: locations,
-        };
-
-        // Make request
-        const res = await fetch(
-          `https://${config.domain}${config.endpoints.api.squad.code.location(
-            squadCode
-          )}`,
-          {
-            method: "post",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify(payload),
-          }
+if (!TaskManager.isTaskDefined(LOCATION_TASK_NAME))
+  TaskManager.defineTask<{ locations: [Location.LocationObject] }>(
+    LOCATION_TASK_NAME,
+    async ({ data, error }) => {
+      if (error) {
+        // Error occurred - check `error.message` for more details.
+        console.error(
+          `An error occured in our background location task, error:${error?.message}`
+        );
+        return;
+      }
+      if (data) {
+        const { locations } = data;
+        // do something with the locations captured in the background
+        console.info(
+          `Retreived ${
+            locations.length
+          } locations from background location task! Locations: ${JSON.stringify(
+            data,
+            null,
+            2
+          )}`
         );
 
-        // If 500 status, no squad exist! stop task.
-        /*if (res.status === 500) {
+        // Send to our websocket/update via fetch request!
+        try {
+          const squadCode = SquadController.getSquadCode();
+          const username = SquadController.getUsername();
+
+          if (!squadCode) return;
+          if (!username) return;
+
+          const payload = {
+            username: username,
+            locations: locations,
+          };
+
+          // Make request
+          const res = await fetch(
+            `https://${config.domain}${config.endpoints.api.squad.code.location(
+              squadCode
+            )}`,
+            {
+              method: "post",
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify(payload),
+            }
+          );
+
+          // If 500 status, no squad exist! stop task.
+          /*if (res.status === 500) {
           await TaskManager.unregisterTaskAsync(LOCATION_TASK_NAME);
           console.info(
             "Unregistered location update task for no valid squad id!!!"
@@ -116,22 +117,22 @@ TaskManager.defineTask<{ locations: [Location.LocationObject] }>(
           throw new Error("Squad not found with that id.");
         }*/
 
-        // Get result
-        const result = await res.json();
+          // Get result
+          const result = await res.json();
 
-        // Do something with result?
-        console.info(
-          `Location update to API result: ${JSON.stringify(result, null, 2)}`
-        );
-      } catch (_err) {
-        const err = _err as Error;
-        console.error(
-          `Failed sending request! Error: ${err?.stack ?? err?.message}`
-        );
+          // Do something with result?
+          console.info(
+            `Location update to API result: ${JSON.stringify(result, null, 2)}`
+          );
+        } catch (_err) {
+          const err = _err as Error;
+          console.error(
+            `Failed sending request! Error: ${err?.stack ?? err?.message}`
+          );
+        }
       }
     }
-  }
-);
+  );
 
 const styles = StyleSheet.create({
   container: {
